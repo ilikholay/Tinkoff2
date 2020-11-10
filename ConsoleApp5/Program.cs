ee16c27b3a4d00730edeca690d08b5976ac10e98
ilikholay <45207297+ilikholay@users.noreply.github.com>
2020-11-10 13:52:32 +0300

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculator
+{
+    public class Program
+    {
+        public interface ICalculator
+        {
+            void Plus(double num);            void Minus(double num);            void Multiply(double num);            void Divide(double num);
+        }
+
+        public class Calculator : ICalculator
+        {
+            public double score;
+            public void Plus(double num)
+            {
+                score += num;
+            }            public void Minus(double num)
+            {
+                score -= num;
+            }            public void Multiply(double num)
+            {
+                score *= num;
+            }            public void Divide(double num)
+            {
+                if (num == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+                score /= num;
+            }
+            public Calculator()
+            {
+                score = 0;
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            var calc = new Calculator();
+            calc.Minus(10);
+            calc.Plus(20.7);
+            Console.WriteLine(calc.score);
+        }
+    }
+}
